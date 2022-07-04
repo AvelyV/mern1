62a6e84fe30f84d24cb9d981d4bd115e2b9a1831
AvelyV <avely.voore@gmail.com>
2022-07-05 10:37:15 +1200

    useEffect for getting all the users to print out

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,29 @@
-
 import './App.css';
+import { useState, useEffect } from 'react';
+import Axios from "axios"
 
 function App() {
+  const [allUsers, setAllUsers] = useState([]);
+
+  useEffect(() => {
+    Axios.get("http://localhost:4000/getUsers")
+      .then((response) => {
+        setAllUsers(response.data)
+      })
+  }, [])
+
   return (
     <div className="App">
       <div className="usersDisplay">
-        
+        {allUsers.map((user) => {
+          return (
+            <div>
+              <h4>Name: {user.name}</h4>
+              <h4>Age: {user.age}</h4>
+              <h4>Email: {user.email}</h4>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
